22309b5f8f97464d5fac8f31fa1ef94f065ff3c8
Leandro Olczyk <leandro.olczyk@hotmail.com>
2026-04-04 14:48:26 +0000

    Adiciona botão de reinício ao jogo Mario Jump e melhora a estilização do menu inicial; ajusta a lógica de controle de jogo e áudio.

--- public/projects/mario-jump/script.js
+++ public/projects/mario-jump/script.js
@@ -1,15 +1,21 @@
+// ----------------- Seleção de elementos -----------------
+
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
 const startMenu = document.querySelector('.start-menu');
 const startBtn = document.querySelector('#start-btn');
+const restartBtn = document.querySelector('#restart-btn');
+const menuTitle = document.querySelector('#menu-title');
+
+// ----------------- Variáveis de estado -----------------
 
 let isJumping = false;
 let isGameOver = false;
 let isGameStarted = false;
 let loop = null;
 
-
+// ----------------- Controle de audio -----------------
 const soundManager = {
     jump: new Audio('./sounds/jump.wav'),
     die: new Audio('./sounds/mario_die.wav'),
@@ -32,6 +38,7 @@ soundManager.jump.volume = 0.3;
 soundManager.die.volume = 0.3;
 soundManager.music.volume = 1;
 
+// ----------------- Controle de jogo -----------------
 
 const jump = (event) => {
     if (!isGameStarted) return;
@@ -50,6 +57,50 @@ mario.addEventListener('animationend', () => {
     isJumping = false;
 });
 
+// ----------------- Lógica de início e fim de jogo -----------------
+
+const endGame = () => {
+    isGameOver = true;
+    isJumping = false;
+    mario.classList.remove('jump');
+
+    soundManager.play('die');
+    soundManager.stop('music');
+
+    clearInterval(loop);
+    
+    startMenu.classList.remove('hidden');
+    menuTitle.innerText = 'Game Over';
+
+    startBtn.style.display = 'none';
+    restartBtn.style.display = 'block';
+}
+
+const restartGame = () => {
+    isGameOver = false;
+    isGameStarted = false;
+    isJumping = false;
+
+    mario.classList.remove('jump');
+
+    mario.src = './Imagens/mario.gif';
+    mario.style.width = '150px';
+    mario.style.marginLeft = '0';
+    mario.style.bottom = '0';
+    mario.style.animation = '';
+
+    pipe.style.animation = '';
+    pipe.style.left = '';
+
+    clouds.style.animationPlayState = 'running';
+
+    startMenu.classList.remove('hidden');
+    menuTitle.innerText = "Mario Jump";
+
+    startBtn.style.display = "block";
+    restartBtn.style.display = "none";
+};
+
 const startGame = () => {
     if (isGameStarted) return;
 
@@ -66,12 +117,11 @@ const startGame = () => {
     loop = setInterval(() => {
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+        
+        const marioWidth = mario.offsetWidth;
+        const pipeWidth = pipe.offsetWidth;
 
-        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-            isGameOver = true;
-
-            soundManager.play('die');
-            soundManager.stop('music');
+        if (pipePosition <= marioWidth * 0.8 && pipePosition > 0 && marioPosition < pipeWidth) {
 
             pipe.style.animation = 'none';
             pipe.style.left = `${pipePosition}px`;
@@ -85,14 +135,18 @@ const startGame = () => {
 
             clouds.style.animationPlayState = 'paused';
 
-            clearInterval(loop);
+            endGame();
         }
     }, 10);
 };
 
+// ----------------- Eventos -----------------
+
 startBtn.addEventListener('click', startGame);
+restartBtn.addEventListener('click', restartGame);
 
 document.addEventListener('keydown', jump);
+
 document.addEventListener('click', (event) => {
     if (!isGameStarted) return;
     if (event.target.id === 'start-btn') return;
